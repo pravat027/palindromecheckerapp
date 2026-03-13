a370e1c86961a78cf9f26b24843a3362cef51f32
pravaat027 <pm1433@srmist.edu.in>
2026-03-13 15:58:35 +0530

    uc8

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,31 +1,67 @@
 import java.util.*;
+import java.util.Scanner;
 public class PalindromeCheckerApp {
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+    static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null)
+            return true;
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            queue.add(ch);
-            stack.push(ch);
-        }
-        boolean isPalindrome = true;
-        while (!queue.isEmpty()) {
-            char qChar = queue.remove();
-            char sChar = stack.pop();
+        Node head = null;
+        Node tail = null;
+        for (char c : input.toCharArray()) {
+            Node newNode = newNode(c);
 
-            if (qChar != sChar) {
-                isPalindrome = false;
-                break;
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
             }
         }
-        if (isPalindrome) {
-            System.out.println("The string is  a Palindrome.");
-        } else {
+        if (isPalindrome(head))
+            System.out.println("The string is a Palindrome.");
+        else
             System.out.println("The string is NOT a Palindrome.");
-        }
         sc.close();
     }
 }
